f6ff9c8e86798e3e5a8845db62db555907dbbe66
Zdenek <374366@fi.muni.cz>
2015-10-04 22:25:32 +0200

    App class added

---
+++ app/src/main/java/uco374366/movio/pv256/fi/muni/cz/courseapp/App.java
@@ -0,0 +1,36 @@
+package uco374366.movio.pv256.fi.muni.cz.courseapp;
+
+
+import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            initStrictMode();
+        }
+    }
+
+    private void initStrictMode() {
+        StrictMode.ThreadPolicy.Builder tpb = new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            tpb.penaltyFlashScreen();
+        }
+        StrictMode.setThreadPolicy(tpb.build());
+
+        StrictMode.VmPolicy.Builder vmpb = new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            vmpb.detectLeakedClosableObjects();
+        }
+        StrictMode.setVmPolicy(vmpb.build());
+    }
+}
