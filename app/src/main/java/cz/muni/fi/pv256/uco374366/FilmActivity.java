a7cc116b5036c6fe5945983bfb03f155791453e7
Zdenek <374366@fi.muni.cz>
2015-10-19 09:31:35 +0200

    Film detail attributes added

--- app/src/main/java/cz/muni/fi/pv256/uco374366/FilmActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/uco374366/FilmActivity.java
@@ -1,7 +1,10 @@
 package cz.muni.fi.pv256.uco374366;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FilmActivity extends Activity {
 
@@ -10,6 +13,16 @@ public class FilmActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film);
         //getActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+
+        TextView textViewTitle = (TextView) findViewById(R.id.title);
+        textViewTitle.setText(intent.getStringExtra("TITLE"));
+
+        TextView textViewReleaseDate = (TextView) findViewById(R.id.releaseDate);
+        textViewReleaseDate.setText(Long.toString(intent.getLongExtra("RELEASE_DATE", -1)));
+
+        ImageView imageViewCover = (ImageView) findViewById(R.id.cover);
+        imageViewCover.setImageResource(intent.getIntExtra("COVER", -1));
     }
 
 }
